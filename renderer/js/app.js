89ab09ff5370a62eaf1ec93790f9c086e2303a99
LaurensHoogenboom <49717394+LaurensHoogenboom@users.noreply.github.com>
2020-03-10 17:39:15 +0100

    header and toolbar front-end

--- renderer/js/app.js
+++ renderer/js/app.js
@@ -1,2 +1,14 @@
+$ = window.jQuery = require('jquery');
+
 const {ipcRenderer} = require('electron')
 const user = require('./user')
+
+//selectbox
+
+$('.select .button').click(function() {
+    $(this).closest('.select').find('.options').each(function() {
+        $(this).toggleClass('expand')
+    })
+
+    $(this).find('.chevron').toggleClass('clicked')
+})
